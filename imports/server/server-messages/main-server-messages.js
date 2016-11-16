ec769b65eaa2fbcce9f185031d079cf3f1c59d19
Gael Flores <flores.gael@gmail.com>
2016-11-16 19:29:10 +0100

    rename

--- imports/server/server-messages/main-server-messages.js
+++ imports/server/server-messages/main-server-messages.js
@@ -1,13 +1,13 @@
 export const MainServerMessages = new Mongo.Collection(null);
 
 
-export const sendServerMessage = (message) => {
+export const sendMainServerMessage = (message) => {
     message.date = new Date();
 
     console.log("sendServerMessage: ", JSON.stringify(message));
     // MainServerMessages.insert(message);
 }
 
-export const cleanupServerMessages = (message) => {
+export const cleanupMainServerMessages = (message) => {
     // MainServerMessages.remove({});
 }
